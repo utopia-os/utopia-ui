f9f9bb3283062b3690ca683594faed1f5cf31b4d
Anton Tranelis <mail@antontranelis.de>
2024-02-26 13:03:17 +0100

    changes to overla page

--- src/Components/Templates/MapOverlayPage.tsx
+++ src/Components/Templates/MapOverlayPage.tsx
@@ -3,7 +3,7 @@ import * as L from 'leaflet';
 import * as React from 'react'
 import { useNavigate } from 'react-router-dom';
 
-export function MapOverlayPage({ children, className, backdrop }: { children: React.ReactNode, className?: string, backdrop?: boolean }) {
+export function MapOverlayPage({ children, className, backdrop, card = true }: { children: React.ReactNode, className?: string, backdrop?: boolean, card?:boolean }) {
 
 
     const closeScreen = () => {
@@ -32,11 +32,9 @@ export function MapOverlayPage({ children, className, backdrop }: { children: Re
     return (
         <div className={`tw-absolute tw-h-full tw-w-full tw-m-auto ${backdrop && "tw-z-[2000]"}`}>
             <div ref={backdropRef} className={`${backdrop && "tw-backdrop-brightness-75"} tw-h-full tw-w-full tw-grid tw-place-items-center tw-m-auto`} >
-                <div ref={overlayRef} className={`tw-card tw-shadow-xl tw-bg-base-100 tw-p-6 ${className && className} ${!backdrop && "tw-z-[2000]"} tw-absolute tw-top-0 tw-bottom-0 tw-right-0 tw-left-0 tw-m-auto`}>
-                    <div className="tw-card-body tw-p-0 tw-h-full">
+                <div ref={overlayRef} className={`${card && "tw-card tw-card-body"} tw-shadow-xl tw-bg-base-100 tw-p-6 ${className && className} ${!backdrop && "tw-z-[2000]"} tw-absolute tw-top-0 tw-bottom-0 tw-right-0 tw-left-0 tw-m-auto`}>
                         {children}
                         <button className="tw-btn tw-btn-sm tw-btn-circle tw-btn-ghost tw-absolute tw-right-2 tw-top-2" onClick={() => closeScreen()}>✕</button>
-                    </div>
                 </div>
             </div>
         </div>

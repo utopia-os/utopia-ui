829d07b8ea5a46d4c7668b07e46309071dcf1f9a
Anton Tranelis <mail@antontranelis.de>
2024-05-03 07:03:46 +0200

    popup button color

--- src/Components/Map/Subcomponents/ItemPopupComponents/PopupButton.tsx
+++ src/Components/Map/Subcomponents/ItemPopupComponents/PopupButton.tsx
@@ -1,14 +1,19 @@
-import * as React from 'react'
 import { Link } from 'react-router-dom'
 import { getValue } from '../../../../Utils/GetValue'
 import { Item } from '../../../../types'
+import { useGetItemTags } from '../../hooks/useTags';
+
 
 export const PopupButton = ({url, parameterField, text, colorField, item} : {url: string, parameterField?: string, text: string, colorField?: string, item? : Item}) => {
+
     let params = new URLSearchParams(window.location.search);
+    const getItemTags = useGetItemTags();
+
+    
 
     
     return (
-        <Link to={`${url}/${parameterField? getValue(item,parameterField):``}?${params}`}><button style={{backgroundColor: `${colorField && getValue(item,colorField)? getValue(item,colorField) : item?.layer?.markerDefaultColor}`}} className="tw-btn tw-text-white tw-btn-sm tw-float-right tw-mt-1">{text}</button></Link>
+        <Link to={`${url}/${parameterField? getValue(item,parameterField):``}?${params}`}><button style={{backgroundColor: `${colorField && getValue(item,colorField)? getValue(item,colorField) : (item && getItemTags(item) && getItemTags(item)[0] && getItemTags(item)[0].color ? getItemTags(item)[0].color : item?.layer?.markerDefaultColor)}`}} className="tw-btn tw-text-white tw-btn-sm tw-float-right tw-mt-1">{text}</button></Link>
         
     )
 }

67883103681412f537cff7cc7374aaac61a64c40
Anton Tranelis <mail@antontranelis.de>
2024-06-16 22:28:32 +0200

    mobile fix

--- src/Components/Map/Subcomponents/Controls/SearchControl.tsx
+++ src/Components/Map/Subcomponents/Controls/SearchControl.tsx
@@ -111,7 +111,7 @@ export const SearchControl = () => {
                     <LocateControl />
                 </div>
                 {hideSuggestions || Array.from(geoResults).length == 0 && itemsResults.length == 0 && tagsResults.length == 0 && !isGeoCoordinate(value) || value.length == 0 ? "" :
-                    <div className='tw-card tw-card-body tw-bg-base-100 tw-p-4 tw-mt-2 tw-shadow-xl tw-overflow-y-auto tw-max-h-[calc(100dvh-152px)]'>
+                    <div className='tw-card tw-card-body tw-bg-base-100 tw-p-4 tw-mt-2 tw-shadow-xl tw-overflow-y-auto tw-max-h-[calc(100dvh-152px)] tw-absolute tw-z-3000'>
                         {tagsResults.length > 0 &&
                             <div className='tw-flex tw-flex-wrap'>
                                 {tagsResults.slice(0, 3).map(tag => (
--- src/Components/Templates/ItemCard.tsx
+++ src/Components/Templates/ItemCard.tsx
@@ -6,27 +6,26 @@ import { useNavigate } from 'react-router-dom'
 import { getValue } from '../../Utils/GetValue'
 import useWindowDimensions from '../Map/hooks/useWindowDimension'
 
-export const ItemCard = ({i,loading, url, parameterField, deleteCallback}:{i:Item, loading: boolean, url:string, parameterField: string, deleteCallback: any}) => {
-    const navigate = useNavigate();
-    const windowDimensions = useWindowDimensions();
+export const ItemCard = ({ i, loading, url, parameterField, deleteCallback }: { i: Item, loading: boolean, url: string, parameterField: string, deleteCallback: any }) => {
+  const navigate = useNavigate();
+  const windowDimensions = useWindowDimensions();
 
   return (
     <div className='tw-cursor-pointer tw-card tw-border-[1px] tw-border-base-300 tw-card-body tw-shadow-xl tw-bg-base-100 tw-text-base-content tw-p-4 tw-mb-4 tw-h-fit' onClick={() => {
       let params = new URLSearchParams(window.location.search);
-      windowDimensions.width >= 786 && navigate(url + getValue(i, parameterField) + `${params ? `?${params}` : ""}`)
-      windowDimensions.width < 786 && navigate("/" + getValue(i, parameterField) + `${params ? `?${params}` : ""}`)
-
-      }}>
-    <HeaderView loading={loading} item={i} api={i.layer?.api} itemAvatarField={i.layer?.itemAvatarField} itemNameField={i.layer?.itemNameField} itemSubnameField={i.layer?.itemSubnameField} editCallback={() => navigate("/edit-item/" + i.id)} deleteCallback={() => deleteCallback(i)}></HeaderView>
-    <div className='tw-overflow-y-auto tw-overflow-x-hidden tw-max-h-64 fade'>
+      if (windowDimensions.width < 786 && i.position)  navigate("/" + getValue(i, parameterField) + `${params ? `?${params}` : ""}`)
+      else navigate(url + getValue(i, parameterField) + `${params ? `?${params}` : ""}`)
+    }}>
+      <HeaderView loading={loading} item={i} api={i.layer?.api} itemAvatarField={i.layer?.itemAvatarField} itemNameField={i.layer?.itemNameField} itemSubnameField={i.layer?.itemSubnameField} editCallback={() => navigate("/edit-item/" + i.id)} deleteCallback={() => deleteCallback(i)}></HeaderView>
+      <div className='tw-overflow-y-auto tw-overflow-x-hidden tw-max-h-64 fade'>
         {i.layer?.itemType.show_start_end &&
-            <StartEndView item={i}></StartEndView>
+          <StartEndView item={i}></StartEndView>
         }
         {i.layer?.itemType.show_text &&
-            <TextView truncate item={i} itemTextField={i.layer?.itemTextField} />
+          <TextView truncate item={i} itemTextField={i.layer?.itemTextField} />
         }
+      </div>
+      <DateUserInfo item={i}></DateUserInfo>
     </div>
-    <DateUserInfo item={i}></DateUserInfo>
-</div>
   )
 }

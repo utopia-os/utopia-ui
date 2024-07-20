b707b8204b59c3ea29a2bd77185ab6985506e28b
Anton Tranelis <mail@antontranelis.de>
2024-07-20 15:23:29 +0200

    optimized wc profiles

--- src/Components/Profile/ProfileSubHeader.tsx
+++ src/Components/Profile/ProfileSubHeader.tsx
@@ -4,16 +4,16 @@ import SocialShareBar from './SocialShareBar';
 const flags = {
     de: (
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5 3" className="tw-w-5 tw-h-3">
-            <rect width="5" height="3" fill="#FFCE00"/>
-            <rect width="5" height="2" fill="#DD0000"/>
-            <rect width="5" height="1" fill="#000000"/>
+            <rect width="5" height="3" fill="#FFCE00" />
+            <rect width="5" height="2" fill="#DD0000" />
+            <rect width="5" height="1" fill="#000000" />
         </svg>
     ),
     at: (
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5 3" className="tw-w-5 tw-h-3">
-            <rect width="5" height="3" fill="#ED2939"/>
-            <rect width="5" height="2" fill="#FFFFFF"/>
-            <rect width="5" height="1" fill="#ED2939"/>
+            <rect width="5" height="3" fill="#ED2939" />
+            <rect width="5" height="2" fill="#FFFFFF" />
+            <rect width="5" height="1" fill="#ED2939" />
         </svg>
     )
 };
@@ -21,14 +21,20 @@ const flags = {
 const statusMapping = {
     'in_planning': 'in Planung',
     'paused': 'pausiert',
+    'active': 'aktiv'
 };
 
 const SubHeader = ({ type, status, url, title }) => (
     <div>
-        {type && <div className="tw-flex tw-items-center tw-mt-4">
-            <span className="tw-text-sm tw-text-gray-600 tw-bg-slate-200 tw-rounded tw-py-1 tw-px-2">{type}{(status && status !== 'active') ? ` (${statusMapping[status]})` : ''}</span>
-        </div>}
-        <div className="tw-mt-4">
+        <div className='tw-float-left tw-mt-4 tw-flex tw-items-center'>
+            {type && <div className="tw-mt-1.5">
+                <span className="tw-text-sm tw-text-gray-600 tw-bg-slate-200 tw-rounded tw-py-1 tw-px-2">{type}</span>
+            </div>}
+            {status  && status!="active" && <div className="tw-mt-1.5">
+                <span className="tw-text-sm tw-text-gray-600 tw-bg-slate-200 tw-rounded tw-py-0.5 tw-px-2 tw-ml-2 tw-inline-flex tw-items-center"><span className={`tw-w-2 tw-h-2  ${ status=="in_planning" && "tw-bg-blue-700"} ${ status=="paused" && "tw-bg-orange-400"} ${ status=="active" && "tw-bg-green-500"} tw-rounded-full tw-mr-1.5`}></span>{statusMapping[status]}</span>
+            </div>}
+        </div>
+        <div>
             <SocialShareBar url={url} title={title} />
         </div>
     </div>

23578502e33adddfc58824198d8526246a0f9b73
Anton Tranelis <mail@antontranelis.de>
2024-07-29 21:52:02 +0200

    give layer and filter control some more padding

--- src/Components/Map/Subcomponents/Controls/LayerControl.tsx
+++ src/Components/Map/Subcomponents/Controls/LayerControl.tsx
@@ -17,10 +17,10 @@ export function LayerControl() {
     const addVisibleLayer = useAddVisibleLayer();
 
     return (
-        <div className="tw-card tw-bg-base-100 tw-shadow-xl tw-mt-2 tw-w-fit tw-pr-4">
+        <div className="tw-card tw-bg-base-100 tw-shadow-xl tw-mt-2 tw-w-fit">
             {
                 open ?
-                    <div className="tw-card-body tw-p-2 tw-w-36 tw-transition-all tw-duration-300">
+                    <div className="tw-card-body tw-pr-4 tw-p-2 tw-w-36 tw-transition-all tw-duration-300">
                         <label className="tw-btn tw-btn-sm tw-rounded-2xl tw-btn-circle tw-btn-ghost hover:tw-bg-transparent tw-absolute tw-right-0 tw-top-0 tw-text-gray-600" onClick={() => {
                             setOpen(false)
                         }}>

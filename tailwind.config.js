12401766ae3389d11e114ccb7609de49dea003d5
Anton Tranelis <mail@antontranelis.de>
2024-07-31 18:32:59 +0200

    animate select position

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,6 +37,16 @@ module.exports = {
         'map': "1.4em"
       }
     },
+    keyframes: {
+      pulseGrow: {
+        '0%, 100%': { transform: 'scale(1.00)' },
+        '80%': { transform: 'scale(1.00)' },
+        '90%': { transform: 'scale(0.95)' },
+      },
+    },
+    animation: {
+      pulseGrow: 'pulseGrow 2s ease-in-out infinite',
+    },
   },
   plugins: [
     require("daisyui"),

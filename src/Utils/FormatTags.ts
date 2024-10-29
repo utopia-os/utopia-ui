ccc52349104bd441315c344b7fdc6e64103b19cb
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2024-10-29 12:21:52 +0100

    test

--- src/Utils/FormatTags.ts
+++
@@ -1,8 +0,0 @@
-export function decodeTag(string : string) {   
-    let formatedTag = string.replace(/_/g, "\u00A0");    
-    return formatedTag = formatedTag.charAt(0).toUpperCase() + formatedTag.slice(1);
-}
-
-export function encodeTag(string : string) {   
-    return string.replace(/\s+/g, "_");    
-}

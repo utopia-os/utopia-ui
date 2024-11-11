1ed2211efa86ba63450e3ebe38f00e209139873d
Anton Tranelis <mail@antontranelis.de>
2024-11-11 14:41:18 +0100

    updated regex for mail detection

--- src/Utils/ReplaceURLs.ts
+++ src/Utils/ReplaceURLs.ts
@@ -1,7 +1,7 @@
 export const urlRegex =
   // eslint-disable-next-line no-useless-escape
   /(^| )(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,10}(:[0-9]{1,10})?(\/.*)?$/gm
-export const mailRegex = /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi
+export const mailRegex = /(?<![[(])([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})(?![\])])/gi
 
 export function fixUrls(message: string): string {
   message = message.replace(urlRegex, function (url) {

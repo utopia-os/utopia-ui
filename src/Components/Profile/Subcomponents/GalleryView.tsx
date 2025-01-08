5ffe44de2ccf7519876d3c07194602b8532d0dc8
Anton Tranelis <mail@antontranelis.de>
2025-01-08 00:31:58 +0000

    implemented gallery widget

--- src/Components/Profile/Subcomponents/GalleryView.tsx
+++ src/Components/Profile/Subcomponents/GalleryView.tsx
@@ -1,23 +1,41 @@
+/* eslint-disable import/no-unassigned-import */
+/* eslint-disable import/order */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { useState } from 'react'
-import Lightbox from 'yet-another-react-lightbox/*'
+// eslint-disable-next-line import/no-named-as-default
+import Lightbox from 'yet-another-react-lightbox'
+import 'yet-another-react-lightbox/styles.css'
+import { RowsPhotoAlbum } from 'react-photo-album'
+import 'react-photo-album/rows.css'
+
+import { useAppState } from '#components/AppShell/hooks/useAppState'
 
 import type { Item } from '#types/Item'
 
 export const GalleryView = ({ item }: { item: Item }) => {
-  const [open, setOpen] = useState(false)
-  // eslint-disable-next-line no-console
-  console.log(item)
+  const [index, setIndex] = useState(-1)
+  const appState = useAppState()
+  const images = item.gallery.map((i, j) => {
+    return {
+      src: appState.assetsApi.url + `${i.directus_files_id.id}.jpg`,
+      width: i.directus_files_id.width,
+      height: i.directus_files_id.height,
+      index: j,
+    }
+  })
   return (
-    <>
-      <button type='button' onClick={() => setOpen(true)}>
-        Open Lightbox
-      </button>
-
-      <Lightbox
-        open={open}
-        close={() => setOpen(false)}
-        slides={[{ src: '/image1.jpg' }, { src: '/image2.jpg' }, { src: '/image3.jpg' }]}
+    <div className='tw-mx-6 tw-mb-6'>
+      <RowsPhotoAlbum
+        photos={images}
+        targetRowHeight={150}
+        onClick={({ index: current }) => setIndex(current)}
       />
-    </>
+
+      <Lightbox index={index} slides={images} open={index >= 0} close={() => setIndex(-1)} />
+    </div>
   )
 }

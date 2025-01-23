28ee520a16a7667d210d29ea60558a76a04119b0
Anton Tranelis <mail@antontranelis.de>
2025-01-23 13:19:06 +0000

    fix more linting

--- examples/tutorial-2-static-layers/src/main.jsx
+++ examples/tutorial-2-static-layers/src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import App from './App.jsx'
-import './index.css'
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

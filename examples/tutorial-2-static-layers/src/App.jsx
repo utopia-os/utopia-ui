c6a8c543c1ce4498f075343126df93c05c3142dd
Anton Tranelis <mail@antontranelis.de>
2025-01-23 12:23:52 +0000

    added basic examples

---
+++ examples/tutorial-2-static-layers/src/App.jsx
@@ -0,0 +1,26 @@
+import { UtopiaMap, Layer } from "utopia-ui"
+import { events, places } from "./sample-data"
+
+function App() {
+
+  return (
+
+    <UtopiaMap center={[50.6, 9.5]} zoom={5} height='100dvh' width="100dvw">
+      <Layer
+        name='events'
+        markerIcon='calendar'
+        markerShape='square'
+        markerDefaultColor='#700'
+        data={events} />
+      <Layer
+        name='places'
+        markerIcon='point'
+        markerShape='circle'
+        markerDefaultColor='#007'
+        data={places} />
+    </UtopiaMap>
+
+  )
+}
+
+export default App

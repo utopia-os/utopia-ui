5f48f4e645f4b3f2ffa96e41217c621fbe7eb229
Anton Tranelis <mail@antontranelis.de>
2025-01-23 13:02:34 +0000

    fix linting

--- examples/tutorial-1-basic-map/eslint.config.js
+++ examples/tutorial-1-basic-map/eslint.config.js
@@ -1,8 +1,8 @@
 import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
+import { configs } from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
 
 export default [
   { ignores: ['dist'] },
@@ -19,20 +19,17 @@ export default [
     },
     settings: { react: { version: '18.3' } },
     plugins: {
-      react,
+      configs,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
+      ...configs.recommended.rules,
+      ...configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
 ]
--- examples/tutorial-2-static-layers/src/App.jsx
+++ examples/tutorial-2-static-layers/src/App.jsx
@@ -1,25 +1,25 @@
-import { UtopiaMap, Layer } from "utopia-ui"
-import { events, places } from "./sample-data"
+import { UtopiaMap, Layer } from 'utopia-ui'
+
+import { events, places } from './sample-data'
 
 function App() {
-
   return (
-
-    <UtopiaMap center={[50.6, 9.5]} zoom={5} height='100dvh' width="100dvw">
+    <UtopiaMap center={[50.6, 9.5]} zoom={5} height='100dvh' width='100dvw'>
       <Layer
         name='events'
         markerIcon='calendar'
         markerShape='square'
         markerDefaultColor='#700'
-        data={events} />
+        data={events}
+      />
       <Layer
         name='places'
         markerIcon='point'
         markerShape='circle'
         markerDefaultColor='#007'
-        data={places} />
+        data={places}
+      />
     </UtopiaMap>
-
   )
 }
 

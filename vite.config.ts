a13339ac476e36820d4d530ba7b83ea8eb5dc4de
Moriz Wahl <moriz.wahl@gmx.de>
2025-02-03 21:47:03 +0100

    add coverage thrasholds

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { configDefaults } from 'vitest/config'
 
 export default defineConfig({
   plugins: [react()],
@@ -7,5 +8,18 @@ export default defineConfig({
     globals: true,
     environment: 'happy-dom',
     setupFiles: ['setupTest.ts'],
+    coverage: {
+      all: true,
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        ...configDefaults.exclude,
+      ],
+      thresholds: {
+        lines: 0,
+        functions: 67,
+        branches: 67,
+        statements: 0,
+      },
+    },
   },
 })

0cb4fd19a9a1274759b6dcf10d92a2e583026d02
Moriz Wahl <moriz.wahl@gmx.de>
2025-02-03 21:20:16 +0100

    include vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'happy-dom'
+    environment: 'happy-dom',
   },
 })
-  

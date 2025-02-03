1a2b2275e52ec8e4a66ad9e3b0851646f5e1ca18
Moriz Wahl <moriz.wahl@gmx.de>
2025-02-03 21:05:25 +0100

    add simple unit test

---
+++ src/Components/Input/TextInput.spec.tsx
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react'
+import { describe, it, expect } from 'vitest'
+
+import { TextInput } from './TextInput'
+
+describe('<TextInput />', () => {
+  it('renders properly', () => {
+    const wrapper = render(<TextInput />)
+    expect(wrapper).toBeTruthy()
+  })
+})

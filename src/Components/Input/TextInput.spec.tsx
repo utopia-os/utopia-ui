22258a62caf0d3efb9835cd93fcb1320c6ab9e0d
Moriz Wahl <moriz.wahl@gmx.de>
2025-02-03 22:40:06 +0100

    fully test text input component

--- src/Components/Input/TextInput.spec.tsx
+++ src/Components/Input/TextInput.spec.tsx
@@ -1,11 +1,32 @@
-import { render } from '@testing-library/react'
-import { describe, it, expect } from 'vitest'
+import { render, screen, fireEvent } from '@testing-library/react'
+import { describe, it, expect, beforeEach, vi } from 'vitest'
 
 import { TextInput } from './TextInput'
 
 describe('<TextInput />', () => {
+  let wrapper = render(<TextInput />)
+
+  beforeEach(() => {
+    wrapper = render(<TextInput />)
+  })
+
   it('renders properly', () => {
-    const wrapper = render(<TextInput />)
     expect(wrapper.container.firstChild).toMatchSnapshot()
+  })
+
+  describe('handleChange', () => {
+    it('calls updateFormValue with new value', () => {
+      const updateFormValue = vi.fn()
+      wrapper.rerender(<TextInput updateFormValue={updateFormValue} />)
+      fireEvent.change(screen.getByRole('textbox'), { target: { value: 'test' } })
+      expect(updateFormValue).toBeCalledWith('test')
+    })
+  })
+
+  describe('labelTitle', () => {
+    it('sets label', () => {
+      wrapper.rerender(<TextInput labelTitle='My Title' />)
+      expect(wrapper.container.firstChild).toMatchSnapshot()
+    })
   })
 })

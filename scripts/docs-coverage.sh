1c966300ecfc3c7ce08aa243f5c099fd37247d11
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-02-04 13:17:44 +0100

    better debug output for doc-coverage script

--- scripts/docs-coverage.sh
+++ scripts/docs-coverage.sh
@@ -4,6 +4,6 @@ COVERAGE=$(sed -nE 's/.*>([0-9]{1,3})%<.*/\1/p' docs/coverage.svg | head -1)
 if (( $COVERAGE >= $COVERAGE_REQUIRED )) then
   exit 0;
 else
-  echo "Coverage: $COVERAGE/$COVERAGE_REQUIRED";
+  echo "Coverage: $COVERAGE/$COVERAGE_REQUIRED%";
   exit 1;
 fi

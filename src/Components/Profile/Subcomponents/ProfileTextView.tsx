990f837aaf436f389908746a0536a00beef08350
Maximilian Harz <maxharz@gmail.com>
2025-02-06 12:22:38 +0100

    Use radash.get to support deep properties in ProfileTextView

--- src/Components/Profile/Subcomponents/ProfileTextView.tsx
+++ src/Components/Profile/Subcomponents/ProfileTextView.tsx
@@ -1,10 +1,12 @@
+import { get } from 'radash'
+
 import { TextView } from '#components/Map'
 
 import type { Item } from '#types/Item'
 
 export const ProfileTextView = ({
   item,
-  dataField,
+  dataField = 'text',
   heading,
   hideWhenEmpty,
 }: {
@@ -13,15 +15,19 @@ export const ProfileTextView = ({
   heading: string
   hideWhenEmpty: boolean
 }) => {
+  const text = get(item, dataField)
+
+  if (typeof text !== 'string') {
+    throw new Error('ProfileTextView: text is not a string')
+  }
+
   return (
     <div className='tw-my-10 tw-mt-2 tw-px-6'>
-      {/* eslint-disable-next-line security/detect-object-injection */}
-      {!(item[dataField] === '' && hideWhenEmpty) && (
+      {!(text === '' && hideWhenEmpty) && (
         <h2 className='tw-text-lg tw-font-semibold'>{heading}</h2>
       )}
       <div className='tw-mt-2 tw-text-sm'>
-        {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, security/detect-object-injection */}
-        <TextView itemId={item.id} rawText={dataField ? item[dataField] : item.text} />
+        <TextView itemId={item.id} rawText={text} />
       </div>
     </div>
   )

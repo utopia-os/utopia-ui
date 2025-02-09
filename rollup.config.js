c6484027c4e5e17b7ab3d8253e84af37cf839952
Anton Tranelis <mail@antontranelis.de>
2025-02-09 19:02:35 +0000

    uddated package.json

--- rollup.config.js
+++ rollup.config.js
@@ -7,8 +7,12 @@ export default [
     input: 'src/index.tsx',
     output: [
       {
-        file: 'dist/index.js',
+        file: 'dist/index.esm.js',
         format: 'esm',
+      },
+      {
+        file: 'dist/index.cjs',
+        format: 'cjs',
       },
     ],
     plugins: [

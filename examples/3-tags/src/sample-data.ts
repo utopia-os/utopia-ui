c02efb9a41815dd06c8eea10a0d03a9de39c7eb3
Anton Tranelis <31516529+antontranelis@users.noreply.github.com>
2025-02-19 14:58:48 +0000

    docs(source): add tags example (#131)
    * added tags example
    
    * changed page title
    
    * updated github workflows to test new example
    
    * fixed yaml syntax
    
    * fixed yaml syntax
    
    * fixed packagenames in package.json

---
+++ examples/3-tags/src/sample-data.ts
@@ -0,0 +1,54 @@
+import { Tag } from "utopia-ui";
+
+export const places = [{
+    "id": "062a1176-f7ee-458d-adba-4a0b3b1ce2a0",
+    "name": "Stadtgemüse",
+    "text": "Stadtgemüse Fulda ist eine Gemüsegärtnerei in Maberzell, die es sich zur Aufgabe gemacht hat, die Stadt und seine Bewohner:innen mit regionalem, frischem und natürlich angebautem Gemüse mittels Gemüsekisten zu versorgen. Es gibt also jede Woche, von Frühjahr bis Herbst, angepasst an die Saison eine Kiste mit schmackhaftem und frischem Gemüse für euch, welche ihr direkt vor Ort abholen könnt. \r\n\r\nhttps://stadtgemuese-fulda.de  #food #permaculture #nature",
+    "position": { "type": "Point", "coordinates": [9.632435, 50.560342] },
+  },
+  {
+    "id": "144e379c-b719-4334-9f0e-de277d3b6d0f",
+    "name": "Weidendom",
+    "text": "free camping #nature",
+    "position": { "type": "Point", "coordinates": [9.438793, 50.560112] },
+  }];
+  
+  export const events = [
+    {
+      "id": "efe00aaa-8b14-47b5-a032-3e0560980c1e",
+      "name": "Hackathon",
+      "text": "still in progress #utopia #map",
+      "position": { "type": "Point", "coordinates": [10.5, 51.62] },
+      "start": "2022-03-25T12:00:00",
+      "end": "2022-05-12T12:00:00",
+    }
+  ]
+
+
+  export const tags : Tag[]= [
+    {
+      "id": "e19f46a7-77a4-4a50-99a2-a942dce843a3",
+      "name": "nature",
+      "color": "#9bc53d"
+    },
+    {
+      "id": "2c2099a6-23ac-4308-b91c-86eefeff3a1d",
+      "name": "utopia",
+      "color": "#c3423f"
+    },
+    {
+      "id": "48b2de97-2b9e-432b-b230-7bdc9a5fb6c0",
+      "name": "map",
+      "color": "#5bc0eb"
+    },
+    {
+      "id": "c88f52e6-357b-45fb-a171-9c2b1dceeb8e",
+      "name": "food",
+      "color": "#6761a8"
+    },
+    {
+      "id": "8928cb92-a3c1-4d83-9495-c2eb4fac0bbe",
+      "name": "permaculture",
+      "color": "#44344f"
+    },
+  ]

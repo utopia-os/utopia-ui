9bd4c215627eda6ee4617f370b7a8569ecf8dfa0
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-02-19 13:42:43 +0100

    fix example2
    for some reason this stopped working on maaster.

--- examples/2-static-layers/src/App.tsx
+++ examples/2-static-layers/src/App.tsx
@@ -1,6 +1,40 @@
 import { UtopiaMap, Layer } from "utopia-ui"
 import { events, places } from "./sample-data"
 
+const itemTypeEvent = {
+  name: "event",
+  show_name_input: false,
+  show_profile_button: false,
+  show_start_end: false,
+  show_start_end_input: false,
+  show_text: false,
+  show_text_input: false,
+  custom_text: "",
+  profileTemplate: [],
+  offers_and_needs: false,
+  icon_as_labels: null,
+  relations: false,
+  template: "TODO",
+  questlog: false,
+}
+
+const itemTypePlace = {
+  name: "event",
+  show_name_input: false,
+  show_profile_button: false,
+  show_start_end: false,
+  show_start_end_input: false,
+  show_text: false,
+  show_text_input: false,
+  custom_text: "",
+  profileTemplate: [],
+  offers_and_needs: false,
+  icon_as_labels: null,
+  relations: false,
+  template: "TODO",
+  questlog: false,
+}
+
 function App() {
   return (
     <UtopiaMap center={[50.6, 15.5]} zoom={5} height='100dvh' width="100dvw">
@@ -9,13 +43,23 @@ function App() {
       markerIcon='calendar'
       markerShape='square'
       markerDefaultColor='#700'
-      data={events} />
+      data={events}
+      menuIcon="calendar"
+      menuColor="#700"
+      menuText="events"
+      itemType={itemTypeEvent}
+      />
     <Layer
       name='places'
       markerIcon='point'
       markerShape='circle'
       markerDefaultColor='#007'
-      data={places} />
+      data={places}
+      menuIcon="point"
+      menuColor="#007"
+      menuText="places"
+      itemType={itemTypePlace}
+      />
   </UtopiaMap>
   )
 }

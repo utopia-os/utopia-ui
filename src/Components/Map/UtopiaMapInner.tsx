949f538947bceec26de6fd6e6f24cef6599837f2
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-02-19 16:41:12 +0100

    revert removal of leaflet.markercluster types
    This results in the utopia-map to no longer load, since some css file is
    no longer reachable

--- src/Components/Map/UtopiaMapInner.tsx
+++ src/Components/Map/UtopiaMapInner.tsx
@@ -199,7 +199,7 @@ export function UtopiaMapInner({
         url='https://tile.osmand.net/hd/{z}/{x}/{y}.png'
       />
       <MarkerClusterGroup
-        ref={(r) => setClusterRef(r)}
+        ref={(r) => setClusterRef(r as any)}
         showCoverageOnHover
         chunkedLoading
         maxClusterRadius={50}

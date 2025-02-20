f6f708aa5cee016d64b1ef01ad3f93a952b1bffd
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-02-20 03:18:31 +0100

    support for svg files
    Support to load svg files and include them as bas64 encoded images in
    the bundle.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 import { configDefaults } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   test: {
     globals: true,
     environment: 'happy-dom',

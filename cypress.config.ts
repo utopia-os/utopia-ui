c76990f343f12383fbca6f6fd9bab07a2e2d2b9d
mahula <lenzmath@posteo.de>
2025-02-24 00:42:55 +0100

    add initialcypress e2e configuration

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,23 @@
+import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor'
+import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esbuild'
+import createBundler from '@bahmutov/cypress-esbuild-preprocessor'
 import { defineConfig } from 'cypress'
+
+async function setupNodeEvents(
+  on: Cypress.PluginEvents,
+  config: Cypress.PluginConfigOptions,
+): Promise<Cypress.PluginConfigOptions> {
+  await addCucumberPreprocessorPlugin(on, config)
+
+  on(
+    'file:preprocessor',
+    createBundler({
+      plugins: [createEsbuildPlugin(config)],
+    }),
+  )
+
+  return config
+}
 
 export default defineConfig({
   component: {
@@ -7,5 +26,13 @@ export default defineConfig({
       bundler: 'vite',
     },
     specPattern: ['**/**/*.cy.{ts,tsx}'],
+  },
+  e2e: {
+    chromeWebSecurity: false,
+    retries: 0,
+    specPattern: 'features/**/*.feature',
+    supportFile: false,
+    video: false,
+    setupNodeEvents,
   },
 })

2b0411a37c574ff79087e6e3b7a74ea2c54ff2f6
Moriz Wahl <moriz.wahl@gmx.de>
2025-02-24 20:15:08 +0100

    fix errors that came by the docs typecheck

--- src/Components/Input/TextAreaInput.spec.tsx
+++ src/Components/Input/TextAreaInput.spec.tsx
@@ -21,16 +21,12 @@ describe('<TextAreaInput />', () => {
         initialTags={[
           {
             color: '#b3242f',
-            date_created: '2024-11-09T07:57:04',
             id: '03b41b63-4530-4754-95cf-0abf8f9db476',
-            map: '3af4863a-7435-4487-aad9-146563ee8e02',
             name: 'Feuer',
-            offer_or_need: null,
-            user_created: null,
           },
         ]}
       >
-        <TextAreaInput updateFormValue={updateFormValue} />
+        <TextAreaInput defaultValue={''} updateFormValue={updateFormValue} />
       </TagsProvider>,
     )
   })
@@ -48,7 +44,7 @@ describe('<TextAreaInput />', () => {
 
   describe('labelTitle', () => {
     it('sets label', () => {
-      wrapper.rerender(<TextAreaInput labelTitle='My Title' />)
+      wrapper.rerender(<TextAreaInput defaultValue={''} labelTitle='My Title' />)
       expect(wrapper.container.firstChild).toMatchSnapshot()
     })
   })

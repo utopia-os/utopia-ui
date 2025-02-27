e74fc083ae2a6cd28cae9a9364af7ed27f44dba8
Anton Tranelis <31516529+antontranelis@users.noreply.github.com>
2025-02-27 20:10:42 +0000

    fix(source): cleanup exports (#168)
    * rollup - fail when typescript has warnings or errors
    
    Currently this is detected when building the docu. Since the developer
    rarely does that the problem is detected on github.
    This change allows the developer to discover the error early by failing
    the build.
    
    * removed unused components and exports
    
    ---------
    
    Co-authored-by: Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>

--- src/Components/Templates/index.tsx
+++ src/Components/Templates/index.tsx
@@ -1,9 +1,7 @@
 export { CardPage } from './CardPage'
 export { TitleCard } from './TitleCard'
 export { MapOverlayPage } from './MapOverlayPage'
-export { MoonCalendar } from './MoonCalendar'
 export { SelectUser } from './SelectUser'
-
 export { OverlayItemsIndexPage } from './OverlayItemsIndexPage'
 export { AttestationForm } from './AttestationForm'
 export { MarketView } from './MarketView'

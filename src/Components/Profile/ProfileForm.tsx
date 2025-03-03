ce2809c6a7b901117a4c9352cb5f75784b00d425
Anton Tranelis <mail@antontranelis.de>
2025-03-03 23:07:17 +0000

    found userType artefacts and removed it

--- src/Components/Profile/ProfileForm.tsx
+++ src/Components/Profile/ProfileForm.tsx
@@ -62,7 +62,6 @@ export function ProfileForm() {
   const hasUserPermission = useHasUserPermission()
   const getItemTags = useGetItemTags()
   const items = useItems()
-  const appState = useAppState()
 
   const [urlParams, setUrlParams] = useState(new URLSearchParams(location.search))
 
@@ -146,8 +145,12 @@ export function ProfileForm() {
   const [template, setTemplate] = useState<string>('')
 
   useEffect(() => {
-    setTemplate(item.layer?.itemType.template ?? appState.userType)
-  }, [appState.userType, item])
+    if (item.layer?.itemType.template) setTemplate(item.layer?.itemType.template)
+    else {
+      const userLayer = layers.find((l) => l.userProfileLayer === true)
+      if (userLayer) setTemplate(userLayer.itemType.template)
+    }
+  }, [item, layers])
 
   return (
     <>

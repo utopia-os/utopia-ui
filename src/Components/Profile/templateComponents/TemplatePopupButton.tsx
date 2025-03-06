3aea858d6f0383f323a75b2484deb2b721121dd5
Maximilian Harz <maxharz@gmail.com>
2025-03-06 18:28:28 +0100

    Add showcase for PopupButton template component

---
+++ src/Components/Profile/templateComponents/TemplatePopupButton.tsx
@@ -0,0 +1,24 @@
+import { useContext } from 'react'
+
+import { PopupButton } from '#components/Map'
+
+import { Templateify } from './Templateify'
+import ItemContext from './TemplateItemContext'
+
+import type { JSXElementConstructor } from 'react'
+
+type OmitItem<T extends keyof React.JSX.IntrinsicElements | JSXElementConstructor<unknown>> = Omit<
+  React.ComponentProps<T>,
+  'item'
+>
+
+/**
+ * @category Map
+ */
+export const TemplatePopupButton = (props: OmitItem<typeof PopupButton>) => {
+  const item = useContext(ItemContext)
+
+  return <PopupButton {...props} item={item} />
+}
+
+export const TemplatePopupButton2 = Templateify(PopupButton)

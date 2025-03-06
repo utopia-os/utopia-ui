00c11a19fa1c389a761fbdf5fd251f7046a36d34
Maximilian Harz <maxharz@gmail.com>
2025-03-06 20:32:11 +0100

    Fix template component type

--- src/Components/Profile/templateComponents/Templateify.tsx
+++ src/Components/Profile/templateComponents/Templateify.tsx
@@ -4,8 +4,6 @@ import ItemContext from './TemplateItemContext'
 
 import type { Item } from '#types/Item'
 
-type OmitItem<T> = Omit<T, 'item'>
-
 export function Templateify<T extends { item?: Item }>(Component: React.ComponentType<T>) {
   const TemplateComponent = (props: T) => {
     const item = useContext(ItemContext)
@@ -13,5 +11,5 @@ export function Templateify<T extends { item?: Item }>(Component: React.Componen
     return <Component {...props} item={item} />
   }
 
-  return TemplateComponent
+  return TemplateComponent as React.ComponentType<Omit<T, 'item'>>
 }

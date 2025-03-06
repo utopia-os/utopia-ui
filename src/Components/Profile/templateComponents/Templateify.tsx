87b47d3ed20b702444af46b856f251fdcf33b149
Maximilian Harz <maxharz@gmail.com>
2025-03-06 19:48:37 +0100

    Templateify exported elements (WIP)

--- src/Components/Profile/templateComponents/Templateify.tsx
+++ src/Components/Profile/templateComponents/Templateify.tsx
@@ -3,17 +3,11 @@ import { useContext } from 'react'
 import ItemContext from './TemplateItemContext'
 
 import type { Item } from '#types/Item'
-import type { JSXElementConstructor } from 'react'
 
-type OmitItem<T extends keyof React.JSX.IntrinsicElements | JSXElementConstructor<unknown>> = Omit<
-  React.ComponentProps<T>,
-  'item'
->
+type OmitItem<T> = Omit<T, 'item'>
 
-export function Templateify<
-  T extends keyof React.JSX.IntrinsicElements | JSXElementConstructor<unknown>,
->(Component: React.JSXElementConstructor<React.PropsWithChildren<{ item?: Item }>>) {
-  const TemplateComponent = (props: OmitItem<T>) => {
+export function Templateify<T extends { item?: Item }>(Component: React.ComponentType<T>) {
+  const TemplateComponent = (props: T) => {
     const item = useContext(ItemContext)
 
     return <Component {...props} item={item} />

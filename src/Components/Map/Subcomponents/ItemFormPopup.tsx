1cc8c19f6f515d38377ac1e57a943efa77e2adcd
Anton Tranelis <mail@antontranelis.de>
2025-03-19 08:42:16 +0000

    fix bug in ItemFormPopup

--- src/Components/Map/Subcomponents/ItemFormPopup.tsx
+++ src/Components/Map/Subcomponents/ItemFormPopup.tsx
@@ -92,9 +92,11 @@ export function ItemFormPopup(props: ItemFormPopupProps) {
       setSpinner(false)
       map.closePopup()
     } else {
-      const item = items.find(
-        (i) => i.user_created?.id === user?.id && i.layer?.id === props.layer.id,
-      )
+      const item = items.find((i) => i.user_created?.id === user?.id && i.layer === props.layer)
+      // eslint-disable-next-line no-console
+      console.log(item)
+      // eslint-disable-next-line no-console
+      console.log(props.layer.id)
 
       const uuid = crypto.randomUUID()
       let success = false

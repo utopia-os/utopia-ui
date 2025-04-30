3a2a28c7cc7208ef40039e0e5d8944f9c42e5642
Anton Tranelis <mail@antontranelis.de>
2025-04-30 10:17:07 +0200

    fix gallery bug

--- src/Components/Profile/Subcomponents/GalleryView.tsx
+++ src/Components/Profile/Subcomponents/GalleryView.tsx
@@ -9,26 +9,25 @@ import type { Item } from '#types/Item'
 export const GalleryView = ({ item }: { item: Item }) => {
   const [index, setIndex] = useState(-1)
   const appState = useAppState()
-  const images = item.gallery?.map((i, j) => {
-    return {
+  const images =
+    item.gallery?.map((i, j) => ({
       src: appState.assetsApi.url + `${i.directus_files_id.id}.jpg`,
       width: i.directus_files_id.width,
       height: i.directus_files_id.height,
       index: j,
-    }
-  })
+    })) ?? []
 
-  if (!images) throw new Error('GalleryView: images is undefined')
+  if (images.length > 0)
+    return (
+      <div className='tw:mx-6 tw:mb-6'>
+        <RowsPhotoAlbum
+          photos={images}
+          targetRowHeight={150}
+          onClick={({ index: current }) => setIndex(current)}
+        />
 
-  return (
-    <div className='tw:mx-6 tw:mb-6'>
-      <RowsPhotoAlbum
-        photos={images}
-        targetRowHeight={150}
-        onClick={({ index: current }) => setIndex(current)}
-      />
-
-      <ReactLightbox index={index} slides={images} open={index >= 0} close={() => setIndex(-1)} />
-    </div>
-  )
+        <ReactLightbox index={index} slides={images} open={index >= 0} close={() => setIndex(-1)} />
+      </div>
+    )
+  else return <></>
 }

9ad54b970e36f3e54817fd2de94953aba8a87210
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-06-04 17:52:53 +0200

    tip tap version 2

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 
 import alias from '@rollup/plugin-alias'
+import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 import { dts } from 'rollup-plugin-dts'
@@ -39,6 +40,10 @@ export default [
       aliasConfig,
       resolve({
         extensions: ['.ts', '.tsx'],
+      }),
+      commonjs({
+        include: /node_modules/,
+        requireReturnsDefault: 'auto',
       }),
       postcss({
         plugins: [],
--- src/Components/Input/RichTextEditor.tsx
+++ src/Components/Input/RichTextEditor.tsx
@@ -1,4 +1,187 @@
-import { useEffect, useRef, useState } from 'react'
+import { Color } from '@tiptap/extension-color'
+import Image from '@tiptap/extension-image'
+import ListItem from '@tiptap/extension-list-item'
+import TextStyle from '@tiptap/extension-text-style'
+import { EditorProvider, useCurrentEditor } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import { useState } from 'react'
+import { Markdown } from 'tiptap-markdown'
+
+import type { EditorEvents } from '@tiptap/react'
+
+const MenuBar = () => {
+  const { editor } = useCurrentEditor()
+
+  if (!editor) {
+    return null
+  }
+
+  return (
+    <div className='control-group tiptap-toolbar'>
+      <div className='button-group'>
+        <button
+          type='button'
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          disabled={!editor.can().chain().focus().toggleBold().run()}
+          className={editor.isActive('bold') ? 'is-active' : ''}
+        >
+          Bold
+        </button>
+        <button
+          type='button'
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          disabled={!editor.can().chain().focus().toggleItalic().run()}
+          className={editor.isActive('italic') ? 'is-active' : ''}
+        >
+          Italic
+        </button>
+        <button
+          type='button'
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          disabled={!editor.can().chain().focus().toggleStrike().run()}
+          className={editor.isActive('strike') ? 'is-active' : ''}
+        >
+          Strike
+        </button>
+        <button
+          type='button'
+          onClick={() => editor.chain().focus().toggleCode().run()}
+          disabled={!editor.can().chain().focus().toggleCode().run()}
+          className={editor.isActive('code') ? 'is-active' : ''}
+        >
+          Code
+        </button>
+        <button type='button' onClick={() => editor.chain().focus().unsetAllMarks().run()}>
+          Clear marks
+        </button>
+        <button type='button' onClick={() => editor.chain().focus().clearNodes().run()}>
+          Clear nodes
+        </button>
+        <button
+          type='button'
+          onClick={() => editor.chain().focus().setParagraph().run()}
+          className={editor.isActive('paragraph') ? 'is-active' : ''}
+        >
+          Paragraph
+        </button>
+        <button
+          type='button'
+          onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+          className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}
+        >
+          H1
+        </button>
+        <button
+          type='button'
+          onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+          className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
+        >
+          H2
+        </button>
+        <button
+          type='button'
+          onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+          className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}
+        >
+          H3
+        </button>
+        <button
+          type='button'
+          onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+          className={editor.isActive('heading', { level: 4 }) ? 'is-active' : ''}
+        >
+          H4
+        </button>
+        <button
+          type='button'
+          onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
+          className={editor.isActive('heading', { level: 5 }) ? 'is-active' : ''}
+        >
+          H5
+        </button>
+        <button
+          type='button'
+          onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
+          className={editor.isActive('heading', { level: 6 }) ? 'is-active' : ''}
+        >
+          H6
+        </button>
+        <button
+          type='button'
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={editor.isActive('bulletList') ? 'is-active' : ''}
+        >
+          Bullet list
+        </button>
+        <button
+          type='button'
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={editor.isActive('orderedList') ? 'is-active' : ''}
+        >
+          Ordered list
+        </button>
+        <button
+          type='button'
+          onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+          className={editor.isActive('codeBlock') ? 'is-active' : ''}
+        >
+          Code block
+        </button>
+        <button
+          type='button'
+          onClick={() => editor.chain().focus().toggleBlockquote().run()}
+          className={editor.isActive('blockquote') ? 'is-active' : ''}
+        >
+          Blockquote
+        </button>
+        <button type='button' onClick={() => editor.chain().focus().setHorizontalRule().run()}>
+          Horizontal rule
+        </button>
+        <button type='button' onClick={() => editor.chain().focus().setHardBreak().run()}>
+          Hard break
+        </button>
+        <button
+          type='button'
+          onClick={() => editor.chain().focus().undo().run()}
+          disabled={!editor.can().chain().focus().undo().run()}
+        >
+          Undo
+        </button>
+        <button
+          type='button'
+          onClick={() => editor.chain().focus().redo().run()}
+          disabled={!editor.can().chain().focus().redo().run()}
+        >
+          Redo
+        </button>
+        <button
+          type='button'
+          onClick={() => editor.chain().focus().setColor('#958DF1').run()}
+          className={editor.isActive('textStyle', { color: '#958DF1' }) ? 'is-active' : ''}
+        >
+          Purple
+        </button>
+      </div>
+    </div>
+  )
+}
+
+const extensions = [
+  Color.configure({ types: [TextStyle.name, ListItem.name] }),
+  // TextStyle.configure({ types: [ListItem.name] }),
+  StarterKit.configure({
+    bulletList: {
+      keepMarks: true,
+      keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+    },
+    orderedList: {
+      keepMarks: true,
+      keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+    },
+  }),
+  Markdown,
+  Image,
+]
 
 interface TextAreaProps {
   labelTitle?: string
@@ -27,15 +210,27 @@ export function RichTextEditor({
   required = true,
   updateFormValue,
 }: TextAreaProps) {
-  const ref = useRef<HTMLTextAreaElement>(null)
+  // const ref = useRef<HTMLTextAreaElement>(null)
   const [inputValue, setInputValue] = useState<string>(defaultValue)
 
-  useEffect(() => {
+  /* useEffect(() => {
     setInputValue(defaultValue)
-  }, [defaultValue])
+  }, [defaultValue]) */
 
-  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const newValue = e.target.value
+  console.log(
+    labelTitle,
+    dataField,
+    labelStyle,
+    containerStyle,
+    inputStyle,
+    defaultValue,
+    placeholder,
+    required,
+    updateFormValue,
+  )
+
+  const handleChange = (props: EditorEvents['update']) => {
+    const newValue = props.editor.storage.markdown.getMarkdown()
     setInputValue(newValue)
     if (updateFormValue) {
       updateFormValue(newValue)
@@ -43,23 +238,16 @@ export function RichTextEditor({
   }
 
   return (
-    <div className={`tw:form-control tw:w-full ${containerStyle ?? ''}`}>
-      {labelTitle ? (
-        <label className='tw:label'>
-          <span className={`tw:label-text tw:text-base-content ${labelStyle ?? ''}`}>
-            {labelTitle}
-          </span>
-        </label>
-      ) : null}
-      <textarea
-        required={required}
-        ref={ref}
-        value={inputValue}
-        name={dataField}
-        className={`tw:textarea tw:textarea-bordered tw:w-full tw:leading-5 ${inputStyle ?? ''}`}
-        placeholder={placeholder ?? ''}
-        onChange={handleChange}
-      ></textarea>
-    </div>
+    <EditorProvider
+      slotBefore={<MenuBar />}
+      extensions={extensions}
+      content={inputValue}
+      onUpdate={handleChange}
+      editorProps={{
+        attributes: {
+          class: 'prose prose-sm sm:prose-base lg:prose-lg xl:prose-2xl m-5 focus:outline-none',
+        },
+      }}
+    ></EditorProvider>
   )
 }

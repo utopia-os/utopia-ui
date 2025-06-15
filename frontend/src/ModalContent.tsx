d73e4886e8d9102dd2a3dc36726c48bc76a5060a
Anton Tranelis <31516529+antontranelis@users.noreply.github.com>
2025-06-15 10:29:39 +0200

    fix(other): fixed lib README badges, moved map README, unpublished map changes, f… (#259)
    * fixed lib README badges, moved map README, unpublished map changes, fixed vulnurability
    
    * limit scope of rollups commonjs plugin

--- frontend/src/ModalContent.tsx
+++ frontend/src/ModalContent.tsx
@@ -2,7 +2,6 @@
 /* eslint-disable @typescript-eslint/restrict-plus-operands */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { useEffect, useState } from 'react'
 import { TextView } from 'utopia-ui'
@@ -45,77 +44,6 @@ export function Welcome1({ clickAction1, map }: ChapterProps) {
   )
 }
 
-export function Welcome2({ clickAction1 }: ChapterProps) {
-  return (
-    <>
-      <h3 className='font-bold text-lg'> Dencentralized Networking</h3>
-      <img className='float-right w-32 mx-4 my-2' src='/markers-circle.svg'></img>
-
-      <p className='py-3'>
-        Find like-minded people, projects and events. In your neighbourhood and wherever you are!
-      </p>
-      <p className='py-3'>Onboard new people, places and events</p>
-      <div className='grid'>
-        <button className='btn place-self-end mt-4' onClick={() => clickAction1!()}>
-          next
-        </button>
-      </div>
-    </>
-  )
-}
-
-export function Welcome3({ clickAction1 }: ChapterProps) {
-  return (
-    <>
-      <h3 className='font-bold text-lg'>Mapping the Change</h3>
-      <p className='py-3'>More and more people are waking up to what's really happening. </p>
-      <p className='py-1'>
-        They are in the process of understanding the potential that is within themselves and within
-        the whole mankind.
-      </p>
-      <img className='float-left w-32 mx-4' src='/3markers-globe.svg'></img>
-
-      <p className='py-1'>
-        Starting to reconnect with our Mother Earth and beginning to question things that long times
-        have been taken for granted.
-      </p>
-      <div className='grid'>
-        <label className='btn place-self-end mt-4' onClick={() => clickAction1!()}>
-          next
-        </label>
-      </div>
-    </>
-  )
-}
-
-export function Welcome4({ clickAction1 }: ChapterProps) {
-  return (
-    <>
-      <h3 className='font-bold text-lg'> Dezentralized Networks </h3>
-
-      <p className='py-3'>
-        Find like-minded people, places and events. In your neighbourhood and wherever you are!
-      </p>
-      <img className='float-right w-32 mx-4 my-2' src='/network.svg'></img>
-
-      <p className='py-1'>
-        Hypnotised, they sit in front of screens in concrete blocks, flooded and disillusioned by
-        irrelevant information.
-      </p>
-
-      <p className='py-1'>
-        From an early age, they are trained to do alienated work and consume unhealthy and
-        meaningless products.
-      </p>
-      <div className='grid'>
-        <button className='btn place-self-end mt-4' onClick={() => clickAction1!()}>
-          next
-        </button>
-      </div>
-    </>
-  )
-}
-
 const close = () => {
   const myModal = document.getElementById('my_modal_3') as HTMLDialogElement
   myModal.close()
@@ -138,34 +66,6 @@ export const ModalContent = ({ map }: { map: any }) => {
         return (
           <Welcome1
             map={map}
-            clickAction1={() => {
-              close()
-              setTimeout(() => {
-                //  setQuestsOpen(true);
-                setChapter(1)
-              }, 1000)
-            }}
-          />
-        )
-      case 2:
-        return (
-          <Welcome2
-            clickAction1={() => {
-              setChapter(3)
-            }}
-          />
-        )
-      case 3:
-        return (
-          <Welcome3
-            clickAction1={() => {
-              setChapter(4)
-            }}
-          />
-        )
-      case 4:
-        return (
-          <Welcome4
             clickAction1={() => {
               close()
               setTimeout(() => {
